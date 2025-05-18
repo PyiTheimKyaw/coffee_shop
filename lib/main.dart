bdd90708211d4535d8f3b3dda70a87d46e716e88
PyiTheimKyaw <92578990+PyiTheimKyaw@users.noreply.github.com>
2025-05-18 20:34:01 +0700

    add permission into ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:coffee_shop/utils/app_router.dart';
 import 'package:coffee_shop/utils/strings.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -10,7 +8,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(DevicePreview(enabled: kReleaseMode, builder: (context) => const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
